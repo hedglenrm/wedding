f0a572ccb1b69cff8ce1af3f6db9279c86ce689c
hedglenrm <hedglenrm@gmail.com>
2019-10-16 20:57:08 -0400

    added therapy-collective stuffs

--- js/scripts.js
+++ js/scripts.js
@@ -84,56 +84,56 @@ $(document).ready(function () {
 
     /***************** Header BG Scroll ******************/
 
-    $(function () {
-        $(window).scroll(function () {
-            var scroll = $(window).scrollTop();
+    // $(function () {
+    //     $(window).scroll(function () {
+    //         var scroll = $(window).scrollTop();
 
-            if (scroll >= 20) {
-                $('section.navigation').addClass('fixed');
-                $('header').css({
-                    "border-bottom": "none",
-                    "padding": "35px 0"
-                });
-                $('header .member-actions').css({
-                    "top": "26px",
-                });
-                $('header .navicon').css({
-                    "top": "34px",
-                });
-            } else {
-                $('section.navigation').removeClass('fixed');
-                $('header').css({
-                    "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-                    "padding": "50px 0"
-                });
-                $('header .member-actions').css({
-                    "top": "41px",
-                });
-                $('header .navicon').css({
-                    "top": "48px",
-                });
-            }
-        });
-    });
+    //         if (scroll >= 20) {
+    //             $('section.navigation').addClass('fixed');
+    //             $('header').css({
+    //                 "border-bottom": "none",
+    //                 "padding": "35px 0"
+    //             });
+    //             $('header .member-actions').css({
+    //                 "top": "26px",
+    //             });
+    //             $('header .navicon').css({
+    //                 "top": "34px",
+    //             });
+    //         } else {
+    //             $('section.navigation').removeClass('fixed');
+    //             $('header').css({
+    //                 "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
+    //                 "padding": "50px 0"
+    //             });
+    //             $('header .member-actions').css({
+    //                 "top": "41px",
+    //             });
+    //             $('header .navicon').css({
+    //                 "top": "48px",
+    //             });
+    //         }
+    //     });
+    // });
     /***************** Smooth Scrolling ******************/
 
-    $(function () {
+    // $(function () {
 
-        $('a[href*=#]:not([href=#])').click(function () {
-            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+    //     $('a[href*=#]:not([href=#])').click(function () {
+    //         if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top - 90
-                    }, 2000);
-                    return false;
-                }
-            }
-        });
+    //             var target = $(this.hash);
+    //             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //             if (target.length) {
+    //                 $('html,body').animate({
+    //                     scrollTop: target.offset().top - 90
+    //                 }, 2000);
+    //                 return false;
+    //             }
+    //         }
+    //     });
 
-    });
+    // });
 
     /********************** Social Share buttons ***********************/
 
@@ -213,7 +213,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var itc_kol = {lat: 39.7744912, lng: -79.5957492};
+    var itc_kol = {lat: 40.209839, lng: -79.766107};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: itc_kol,
